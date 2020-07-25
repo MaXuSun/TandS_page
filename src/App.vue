ea507efa3c6332522e705cd55116e15b8a02c514
maxusun <2372883821@qq.com>
2020-07-25 20:16:27 +0800

    添加底部信息

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="router-view">
-        <router-view></router-view>
+        <router-view/>
     </div>
   </div>
 </template>
--- src/views/Display.vue
+++ src/views/Display.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-        <Navigate/>
-        <router-view/>
+        <el-container >
+            <Navigate/>
+            <el-main><router-view/></el-main>
+            <el-footer>
+                <div class="footer">
+                    {{foot_msg}}
+                </div>
+            </el-footer>
+        </el-container>
     </div>
 </template>
 
@@ -9,10 +16,26 @@
     import Navigate from "@/components/Navigate";
     export default {
         name: "Display",
-        components:{Navigate}
+        components:{Navigate},
+        data(){
+            return {
+                foot_msg:'我是底部信息，可以从后端获得或者写死'
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .el-main{
+        background-color: #ededed;
+        height: 700px;
+    }
+    .el-footer{
+        background-color: #545C64;
+    }
+    .footer{
+        text-align: center;
+        margin-top: 10px;
+        color: white;
+    }
 </style>
